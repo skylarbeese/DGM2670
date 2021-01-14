046b372a74fe7f003acb91d8edbddb3a313b9bde
skylarbeese <skylarbeese@gmail.com>
2021-01-13 20:50:33 -0700

    js putda

---
+++ bed-and-brackfist/main.js
@@ -0,0 +1,7 @@
+document.querySelector('comp').innerHtml = "Joes Bed and breakfist"
+
+let userName = prompt('what is you name?')
+
+let message = 'Hello ' + userName + 'this is your username'
+
+document.querySelector('mess').innerHtml = message
